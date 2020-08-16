ec465a5ec5cd332c5f82bcd61ea3260a96aea6ae
Bill_Yang <ym13883407586@outlook.com>
2020-08-16 19:05:40 +0800

    complete closing issue

--- src/notify.cpp
+++ src/notify.cpp
@@ -19,9 +19,7 @@ bool Notify(const int &option,const GroupMessageEvent &event,const json &issue) 
     if(option==0) { //新issue，发送给admin
         if(admin==-1)return false;
         try { //读取配置
-            logging::debug("admin",to_string(admin));
             Message msg=config["notification"]["newissue"].as<string>();
-            logging::debug("newissue",msg);
             msg=replace_all_distinct(msg,"${id}",to_string(issue["id"].get<int>()));
             msg=replace_all_distinct(msg,"${title}",issue["title"].get<string>());
             msg=replace_all_distinct(msg,"${group}",to_string(issue["group"].get<int64_t>()));
@@ -32,13 +30,32 @@ bool Notify(const int &option,const GroupMessageEvent &event,const json &issue) 
             struct tm tm;
             localtime_s(&tm,&t);
             msg=replace_all_distinct(msg,"${time}",to_string(tm.tm_year+1900)+"年"+to_string(tm.tm_mon+1)+"月"+to_string(tm.tm_mday)+"日"+to_string(tm.tm_hour)+"时"+to_string(tm.tm_min)+"分"+to_string(tm.tm_sec)+"秒");
-            logging::debug("newissue2",msg);
             send_private_message(admin,msg);
             return true;
         } catch (ApiError &err) {
             logging::warning("加载数据","读取配置失败！错误码："+to_string(err.code));
             return false;
         }
+    } else if(option==1) { //关闭issue，发送给所有参与者
+        int floors=issue["floors"].get<int>();
+        set<int64_t> participants;
+        for(int i=1; i<=floors; i++) {
+            int64_t id=issue["floor"+to_string(i)]["author"].get<int64_t>();
+            if(participants.count(id)!=0||id==event.user_id)continue;
+            participants.insert(id);
+        }
+
+        Message msg=config["notification"]["closed"].as<string>();
+        msg=replace_all_distinct(msg,"${id}",to_string(issue["id"].get<int>()));
+        msg=replace_all_distinct(msg,"${title}",issue["title"].get<string>());
+        msg=replace_all_distinct(msg,"${group}",to_string(issue["group"].get<int64_t>()));
+        GroupMember gm=get_group_member_info(issue["group"].get<int64_t>(),event.user_id);
+        msg=replace_all_distinct(msg,"${author}",gm.card+"("+to_string(gm.user_id)+")");
+        time_t t=issue["floor"+to_string(floors)]["time"].get<time_t>();
+        struct tm tm;
+        localtime_s(&tm,&t);
+        msg=replace_all_distinct(msg,"${time}",to_string(tm.tm_year+1900)+"年"+to_string(tm.tm_mon+1)+"月"+to_string(tm.tm_mday)+"日"+to_string(tm.tm_hour)+"时"+to_string(tm.tm_min)+"分"+to_string(tm.tm_sec)+"秒");
+        for(auto id:participants)send_private_message(id,msg);
     }
 
     return false;
--- src/response.cpp
+++ src/response.cpp
@@ -10,6 +10,20 @@ extern string commands[COMMAND_AMOUNT+1];
 extern int match_method[COMMAND_AMOUNT+1]; //1模糊 0精确
 extern int priority_requied[COMMAND_AMOUNT+1]; //0全体成员 1管理员或发起者 2仅管理员
 
+int get_id_by_title(const json &data,const string &title) {
+    int number=data["ammount"].get<int>();
+    for(int i=1; i<=number; i++)if(to_string(i)==title)return i; //使用id，直接返回
+    int Min=INT_MAX,id=-1;
+    for(int i=1; i<=number; i++) {
+        string issuetitle=data["issue"+to_string(i)]["title"].get<string>();
+        if(issuetitle.find(title)!=string::npos&&issuetitle.length()<Min) {
+            Min=issuetitle.length();
+            id=i;
+        }
+    }
+    return id;
+}
+
 bool Response(const int &id,const GroupMessageEvent &event) {
     json data;
     try { //读取数据
@@ -48,8 +62,11 @@ bool Response(const int &id,const GroupMessageEvent &event) {
             send_group_message(event.group_id,MessageSegment::at(event.user_id)+"格式错误，正确格式为"+commands[0]+"#\"issue名\"#\"内容\"");
             return false;
         }
+        cmd[1]=replace_all_distinct(cmd[1],"[cap3]","#"); //转义
+        cmd[2]=replace_all_distinct(cmd[2],"[cap3]","#"); //转义
         data["issue"+to_string(number)]["title"]=cmd[1];
         data["issue"+to_string(number)]["id"]=number;
+        data["issue"+to_string(number)]["status"]="Open";
         data["issue"+to_string(number)]["group"]=event.group_id;
         data["issue"+to_string(number)]["floors"]=1;
         data["issue"+to_string(number)]["floor1"]["content"]=cmd[2];
@@ -64,6 +81,47 @@ bool Response(const int &id,const GroupMessageEvent &event) {
 
         Notify(0,event,data["issue"+to_string(number)]); //推送消息
         return true;
+    } else if(id==1) { //关闭issue
+        msg=replace_all_distinct(msg,"\\#","[cap3]"); //转义
+        vector<string> cmd=stringSplit(msg,"#");
+        if(cmd.size()!=2) {
+            send_group_message(event.group_id,MessageSegment::at(event.user_id)+"格式错误，正确格式为"+commands[1]+"#\"issue名\"");
+            return false;
+        }
+        cmd[1]=replace_all_distinct(cmd[1],"[cap3]","#"); //转义
+        int id=get_id_by_title(data,cmd[1]);
+        if(id==-1) {
+            send_group_message(event.group_id,MessageSegment::at(event.user_id)+"不存在该issue");
+            return false;
+        }
+
+        json issue=data["issue"+to_string(id)];
+        issue["status"]="Closed";
+        int floors=issue["floors"].get<int>()+1;
+        issue["floors"]=floors;
+        issue["floor"+to_string(floors)]["content"]="Closed this issue";
+        issue["floor"+to_string(floors)]["author"]=event.user_id;
+        issue["floor"+to_string(floors)]["time"]=time(NULL);
+
+        GroupRole role=get_group_member_info(event.group_id,event.user_id).role;
+        try {
+            if(role==GroupRole::MEMBER && event.user_id!=issue["floor1"]["author"].get<int64_t>()) { //权限不足
+                send_group_message(event.group_id,MessageSegment::at(event.user_id)+"你的权限不足！");
+                return false;
+            }
+
+            data["issue"+to_string(id)]=issue;
+
+            ofstream os(ansi(dir::app()+"groups\\"+to_string(event.group_id)+".json"));
+            os << data.dump(4) << endl;
+            os.close();
+
+            Notify(1,event,issue); //推送消息
+        } catch (nlohmann::detail::type_error &err) { //不存在
+            logging::warning("关闭issue","json储存数据不全，指令响应失败");
+            return false;
+        }
     }
+
     return false;
 }
