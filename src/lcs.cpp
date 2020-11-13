44fbe9a30cf6bc604f35bd0744b8b07dd80c4189
Bill_Yang <ym13883407586@outlook.com>
2020-11-13 20:54:16 +0800

    fixed global memory

--- src/lcs.cpp
+++ src/lcs.cpp
@@ -2,10 +2,9 @@
 
 #include "headers.h"
 
-int f[2005][2005];
-
 int lcs(const wstring &a,const wstring &b) {
-    if(a.length()>2000||b.length()>2000)return 0;
+    int f[205][205];
+    if(a.length()>203||b.length()>203)return 0;
     memset(f,0,sizeof(f));
     for(int i=1; i<=a.length(); i++)
         for(int j=1; j<=b.length(); j++) {
